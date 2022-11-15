b86b12306ff9445bde257fa18fef52a614f90392
Aner <anercoric24@gmail.com>
2022-11-15 16:19:36 +0100

    add html

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
-<?php
-echo 'Ovo je moja includes stranica';
+<div id="post-container"></div>
+<script>function getPosts(){
+        var postContainer=document.getElementById("post-container");
+        var ourRequest = new XMLHttpRequest();
+        ourRequest.open("GET","https://rosins-dev.digiqal.de/wp-json/wp/v2/posts");
+        ourRequest.onload = function (){
+            if(ourRequest.status >= 200 && ourRequest.status < 400){
+                var data =JSON.parse(ourRequest.response);
+//console.log(data);
+                createHTML(data);
+            }else{
+                console.log("We connected to the server, but server returns error");
+            }
+        }
+        ourRequest.onerror=function (){
+            console.log('Connection error');
+        }
+        ourRequest.send();
+    }
+    function createHTML(postData){
+        var postHTML='';
+        for(var i=0; i<postDta.length;i++){
+// console.log(postData[i].title.rendered);
+            postHTML +='<h2>'+postData[i].title.rendered+'</h2>';
+            postHTML +='<h2>'+postData[i].content.rendered+'</h2>';
+        }
+        postContainer.innerHTML=postHTML;
+    }
+    getPosts();
+</script>
