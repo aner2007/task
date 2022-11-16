fe9dc4f9bbe661c8c99d6435b85c91187b4c9dce
Aner <anercoric24@gmail.com>
2022-11-16 01:28:18 +0100

    remove php api

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 
 <?php include 'includes/header.php'; ?>
-<?php include 'php/api.php'; ?>
 <script src="js/api.js"></script>
 <div id="post-container"></div>
 
+
+
+<script> getPosts();</script>
 
 <?php include 'includes/footer.php'; ?>
