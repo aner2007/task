8f2b9a10ae2753af5576af672805b8b79f2a0f38
Aner <anercoric24@gmail.com>
2022-11-16 14:06:58 +0100

    add header and footer

--- includes/header.php
+++ includes/header.php
@@ -1,2 +1,8 @@
-<?php
-echo 'This is header';
+<div class="header">
+    <a href="#default" class="logo">CompanyLogo</a>
+    <div class="header-right">
+        <a class="active" href="#home">Home</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php include 'includes/header.php'; ?>
 <?php include 'css/custom.css'; ?>
+<?php include 'css/footer.css'; ?>
 <script src="js/api.js"></script>
 <div id="post-container"></div>
 <script> getPosts();</script>
