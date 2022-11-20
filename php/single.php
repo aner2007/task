07c6f1c0bc8092dfd5fcbb586cdae1965df88e8b
Aner <anercoric24@gmail.com>
2022-11-20 02:00:05 +0100

    add subscribe

--- php/single.php
+++ php/single.php
@@ -1,7 +1,7 @@
 <?php
 $curl=curl_init();
 curl_setopt($curl,CURLOPT_URL,'https://rosins-dev.digiqal.de/wp-json/wp/v2/posts?_embed');
-//curl_setopt($curl,CURLOPT_URL,'https://rosins-dev.digiqal.de/wp-json/wp/v2/posts?categories=');
+//curl_setopt($curl,CURLOPT_URL,'https://rosins-dev.digiqal.de/wp-json/wp/v2/posts?per_page=100&categories=');
 curl_setopt($curl,CURLOPT_SSL_VERIFYPEER,false);
 curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
 
@@ -12,10 +12,20 @@ $data=json_decode($result);
 
     <ul id="paginated-list" data-current-page="1" aria-live="polite">
         <?php
+        $html5="";
+        $html6="";
+        $html3="";
         foreach($data as $value){
+         if($value->_embedded->{'wp:term'}[0][0]->id == 5 ){
+             $html5.="<li><div class='post-main'><img src=".$value->_embedded->{'wp:featuredmedia'}[0]->source_url." class='post-images' width=350 height=250>";
+             $html5.='<div class="marketing"><span>MARKETING</span><span class="min-read">5min Read</span></div>';
+             $html5.='<h2>'.$value->title->rendered.'</h2>';
+             $html5.='<div class="content-text"><p>'.$value->content->rendered.'</p></div>';
+             $html5.='<a href="'.$value->link.'" class="read-more">Read full article</a></div></li>';
+         }
+
 
             $html='';
-            $html.='<h2> ID category: '.$value->_embedded->{'wp:term'}[0][0]->id.'</h2>';
             $html.="<li><div class='post-main'><img src=".$value->_embedded->{'wp:featuredmedia'}[0]->source_url." class='post-images' width=350 height=250>";
             $html.='<div class="marketing"><span>MARKETING</span><span class="min-read">5min Read</span></div>';
             $html.='<h2>'.$value->title->rendered.'</h2>';
